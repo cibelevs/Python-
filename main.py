6ef89f3c68efabd3441e598b1ab55f8bf685811c
Cibele Vale <cibelevale.estudante@gmail.com>
2025-02-13 17:25:28 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#importando a biblioteca 
+from Bio import SeqIO
+
+#Carregando arquivos 
+input_file = "ZIKV.fasta"
+output_file = "Filtro_seqenv.fasta"
+tam_min = 1501
+
+with open (output_file, "w") as output:
+    for record in SeqIO.parse(input_file, "fasta"):
+        if len(record.seq) >= tam_min:
+            SeqIO.write(record, output, "fasta")
+
+print(f"Sequencias filtradas estão salvas em {output_file}.")
+
